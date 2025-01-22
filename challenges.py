42d8c6b006cfb272362f9acc533a0ea6fd6ef8fe
nightro00 <jjuliodm@gmail.com>
2025-01-22 18:35:19 -0400

    contando palabras solution

--- challenges.py
+++ challenges.py
@@ -15,31 +15,6 @@
 
 
 
-#CONTANDO PALABRAS
-
-"""
-/*
- * Crea un programa que cuente cuantas veces se repite cada palabra
- * y que muestre el recuento final de todas ellas.
- * - Los signos de puntuación no forman parte de la palabra.
- * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
- * - No se pueden utilizar funciones propias del lenguaje que
- *   lo resuelvan automáticamente.
- */
-"""
-
-def contando_palabras(paragraph):
-
-    paragraph_list = paragraph.replace(".", "").replace(",", "").replace("\'", "").split(" ")
-
-    for word in paragraph_list:
-        count_word = 0
-        for word2 in paragraph_list:
-            if word == word2:
-                count_word = count_word + 1
-        print(f"{word} - {count_word}")
-
-#print(contando_palabras("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."))
 
 #DECIMAL A BINARIO
 """
