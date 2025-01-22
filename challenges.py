84461a905fb6082b6f84781e5304dd05e5866385
nightro00 <jjuliodm@gmail.com>
2025-01-22 18:36:33 -0400

    decimal a binario solution

--- challenges.py
+++ challenges.py
@@ -16,33 +16,6 @@
 
 
 
-#DECIMAL A BINARIO
-"""
-/*
- * Crea un programa se encargue de transformar un número
- * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
- */
-"""
-def decimal_binario(num):
-    new_num = num
-    par_impar_list = []
-
-    while(new_num > 1):
-        if(new_num == num and new_num % 2 == 0):
-            par_impar_list.append(0)
-        elif(new_num == num and new_num % 2 != 0):
-            par_impar_list.append(1)
-
-        new_num = int(new_num / 2)
-        if(new_num % 2 == 0):
-            par_impar_list.append(0)
-        elif(new_num % 2 != 0):
-            par_impar_list.append(1)
-
-    return par_impar_list
-        
-
-#print(decimal_binario(420)[::-1])
 
 #CODIGO MORSE
 
