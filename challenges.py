98d750635c6831e7ff15bf58de8d24196f502aad
nightro00 <jjuliodm@gmail.com>
2025-01-22 18:33:20 -0400

    invirtiendo cadena solution

--- challenges.py
+++ challenges.py
@@ -13,18 +13,7 @@
  *   imagen de 1920*1080px.
  */"""
 
-#INVIRTIENDO CADENAS
-"""
-/*
- * Crea un programa que invierta el orden de una cadena de texto
- * sin usar funciones propias del lenguaje que lo hagan de forma automática.
- * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
- */"""
 
-def invirtiendo_cadena(word):
-    print(word[::-1])   
-
-#invirtiendo_cadena("Hola mundo")
 
 #CONTANDO PALABRAS
 
